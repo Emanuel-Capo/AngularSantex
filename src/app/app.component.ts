421c21ae0260ab2151dda81dfe6ae40c1a691e2f
Emanuel-Capo <emanuel_c400@hotmail.com>
2023-07-13 22:07:01 -0300

    terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+type Products = {
+  name: string,
+  price: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularXAcademy';
+  
+  techProds: Products[] = [
+    { name: "monitor", price: 12300},
+    { name: "teclado", price: 1800},
+    { name: "mouse", price: 700},
+    { name: "cpu", price: 32500},
+    { name: "parlante", price: 3500},
+    { name: "impresora", price: 7500},
+    { name: "disco externo", price: 9000},
+    { name: "memoria ram", price: 11200},
+  ]
+
+  show: boolean = true;
+
+  cambio = () => this.show = !this.show
 }
